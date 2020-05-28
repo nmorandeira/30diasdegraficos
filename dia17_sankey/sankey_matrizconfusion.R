a2b78e92bef49797b572060f0e1fc00190901573
Natalia M <nmorandeira@unsam.edu.ar>
2020-05-28 19:14:23 -0300

    Dia 17 - Gráfico de Sankey para evaluar una clasificación de ambientes con datos satelitales

---
+++ dia17_sankey/sankey_matrizconfusion.R
@@ -0,0 +1,56 @@
+#### 30díasdegráficos día 17 #####
+#### Gráfico de Sankey y su utilidad para visualizar matrices de confusión ####
+
+# Datos de Mariela Rajenwerc
+# Gráfico: Natalia Morandeira
+# Parte del script se basa en el código de: Stephanie Orellana (@sporella)
+
+## Cargar librerías ####
+
+library(tidyverse)
+
+## Cargar y procesar datos ####
+
+#La tabla de origen es una típica matriz de confusión: 
+# una matriz cuadrada donde para cada píxel (o polígono) se indica
+# en las filas a qué clase de información corresponde (valor observado o "verdad terrena")
+# y en las columnas en qué clase fue clasificado (valor predico o clasificado)
+
+library(readr)
+matriz_conf1 <- read_csv(file = "data/class1.csv")
+
+library(tidyr)
+
+#lo paso de matriz cuadrada a listado
+list_conf1 <- matriz_conf1 %>% 
+  pivot_longer(Agua:Sauce, "Clasificado")
+head(list_conf1)
+
+### plot #########
+library(ggplot2)
+library(ggalluvial)
+library(wesanderson)
+
+pal <-wes_palette("Moonrise3", n = 5)
+
+
+
+ggplot(data = list_conf1, aes(axis2 = Observado, axis1 = Clasificado, y = value)) +
+  geom_alluvium(aes(fill = Clasificado), show.legend = FALSE) +
+  geom_stratum(aes(fill = Clasificado), colour = "grey33", alpha = 0.2, show.legend = FALSE) +
+  geom_text(size = 4, stat = "stratum", infer.label = TRUE) +
+  scale_fill_manual(values = pal) +
+  scale_x_discrete(limits = c("Observado", "Clasificado"),  expand = c(.2, .05)) +
+  scale_y_continuous(expand = c(0,0)) +
+  labs(title = "Evaluación de una clasificación de ambientes con imágenes satelitales",
+       subtitle = "Clasificación de humedales del Bajo Delta del Paraná (Buenos Aires, Argentina) \ncon imágenes SAR Sentinel-1. Datos: Mariela Rajngewerc") +
+    theme(axis.text.y   = element_blank(),
+          axis.text.x   = element_text(size = 12, face= "bold"),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(), 
+    panel.background = element_rect(size = 0, fill=NA), 
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5))
+
+ggsave("sankey.png",width = 8, height = 5, dpi = 300)
+
